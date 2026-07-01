d27f88a89177b148c679152ca730b3080051fc74
a1l1ke <onetenthplease@gmail.com>
2026-07-01 10:14:57 +0900

    SecureServlet에 01_secure.jsp 포워딩 로직 구현 및 JSP 파일 생성

--- src/main/java/com/example/jsp/step1/SecureServlet.java
+++ src/main/java/com/example/jsp/step1/SecureServlet.java
@@ -1,8 +1,21 @@
 package com.example.jsp.step1;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet("/secure")
 public class SecureServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // /secure라는 가상경로를 요청 받았을 때 주소창 및 요청 객체의 변경 없이
+        // 연동되는 jsp로 이동시킨다
+        // webapp 안에 있는 경로를 인식함
+        req.getRequestDispatcher("01_secure.jsp") // webapp 경로 기준 jsp로 이동
+                .forward(req, resp); // 전달.
+    }
 }
---
+++ src/main/webapp/01_secure.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Unsecured</title>
+</head>
+<body>
+<h1>
+    Unsecured
+</h1>
+</body>
+</html>
