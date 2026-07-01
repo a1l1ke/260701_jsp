21fbf9c71b2d3d5f8fdf8519d6f17518e215c1a0
a1l1ke <onetenthplease@gmail.com>
2026-07-01 10:10:14 +0900

    기존 템플릿(HelloServlet, index.jsp) 삭제 및 SecureServlet 추가

---
+++ src/main/java/com/example/jsp/step1/SecureServlet.java
@@ -0,0 +1,8 @@
+package com.example.jsp.step1;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/secure")
+public class SecureServlet extends HttpServlet {
+}
